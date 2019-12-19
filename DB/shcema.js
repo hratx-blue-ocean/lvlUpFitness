a93f66b3249f799200ccba2a689ec61f3f87f0cd
shaun <shauncarr2010@hotmail.com>
2019-12-19 15:25:25 -0600

    cleaned code

--- DB/shcema.js
+++ DB/shcema.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/lvlupdb', {useNewUrlParser: true});
+const chickienuggs = require('./seeds/Example')
 
 const db = mongoose.connection;
+
+mongoose.connect('mongodb://127.0.0.1:27017/lvlupdb', {useNewUrlParser: true});
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('were connected')
 });
 
-// Example on how to set up the seed
-
-
-
-
-const chickienuggs = require('./seeds/meals')
 
 chickienuggs.save( (err, chickienuggs) => {
     if(err) console.log(err);
