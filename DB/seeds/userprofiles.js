d51e7b1818a86dbc23a87d80f522d9d77d15119f
shaun <shauncarr2010@hotmail.com>
2019-12-19 17:15:54 -0600

    user profile collection schema done

--- DB/seeds/userprofiles.js
+++ DB/seeds/userprofiles.js
@@ -1,16 +1,30 @@
 const mongoose = require('mongoose');
 
 const profileSchema = new mongoose.Schema ({
+    profilePicture: {data: Buffer, contentType: String},
     username: String,
     email: String,
     password: String,
-    savedWorkouts: [{}],
-    meals: [{}]
+    savedWorkouts: [{id: String, Name: String}],
+    savedMeals: [{id: String, Name: String}],
+    favoriteWorkouts: [{id: String, Name: String}],
+    favoriteMeals: [{id: String, Name: String}],
+    createdWorkouts: [{Name: String, Info: String}]
 });
 
 const userProfile = mongoose.model('userProfile', profileSchema);
 
-const profiles = new userProfile({})
+const profiles = new userProfile({
+    profilePicture: {},
+    username: 'Black Nasa',
+    emai: 'blacknasa@nasa.org',
+    password: 'password',
+    savedWorkouts: [{}],
+    savedMeals: [{}],
+    favoriteWorkouts: [{}],
+    favoriteMeals: [{}],
+    createdWorkouts: [{Name: 'Moon Jump', Info: 'Jump around on the moon and call the empire trash' }]
+});
 
 
 module.exports = profiles
