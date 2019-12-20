ec9dd8f58e302f7f9b89542fd46bcceecf5098db
Franicisco Leyva <surefirefjl@gmail.com>
2019-12-20 16:48:30 -0600

    cardio data good

--- DB/shcema.js
+++ DB/shcema.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const profiles = require('./seeds/userprofiles');
-const config = require('./config')
+//const config = require('./config')
 const {bwFullBody,bwLegs,bwChest,bwBack,bwArms,bwShoulders,bwCore} = require('./seeds/bodyWeightDB');
 const {carbMeals,proteinPlans,dietPlans } = require('./seeds/mealPlans')
+const { cardioJumpRope, cardioStrengthCircuit, cardioDistanceRunning, cardioSprints, cardioWalking, cardioCycling, cardioSwimming} = require('./seeds/cardio')
 const db = mongoose.connection;
 
 // mongoose.connect(`mongodb+srv://shauncarr22:${config}@lvlupfitdb-lef31.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true});
@@ -54,17 +55,46 @@ db.once('open', () => {
 //     console.log('bwCore saved');
 // });
 
-carbMeals.save( (err, carbMeals) => {
-    if(err) console.error(err);
-    console.log('carbMeals saved');
-});
+// carbMeals.save( (err, carbMeals) => {
+//     if(err) console.error(err);
+//     console.log('carbMeals saved');
+// });
 
-proteinPlans.save( (err,proteinPlans) => {
-    if(err) console.error(err);
-    console.log('proteinPlans saved');
-});
+// proteinPlans.save( (err,proteinPlans) => {
+//     if(err) console.error(err);
+//     console.log('proteinPlans saved');
+// });
 
-dietPlans.save( (err, dietPlans) => {
-    if(err) console.error(err);
-    console.log('dietPlans saved');
-});
+// dietPlans.save( (err, dietPlans) => {
+//     if(err) console.error(err);
+//     console.log('dietPlans saved');
+// });
+
+// cardioJumpRope.save( (err, cardioJumpRope) => {
+//     if(err) console.error(err);
+//     console.log('cardioJumpRope saved');
+// });
+// cardioCycling.save( (err, cardioCycling) => {
+//     if(err) console.error(err);
+//     console.log('cardioCycling saved');
+// });
+// cardioDistanceRunning.save( (err, cardioDistanceRunning) => {
+//     if(err) console.error(err);
+//     console.log('cardioDistanceRunning saved');
+// });
+// cardioSprints.save( (err, cardioSprints) => {
+//     if(err) console.error(err);
+//     console.log('cardioSprints saved');
+// });
+// cardioSwimming.save( (err, cardioSwimming) => {
+//     if(err) console.error(err);
+//     console.log('cardioSwimming saved');
+// });
+// cardioWalking.save( (err, cardioWalking) => {
+//     if(err) console.error(err);
+//     console.log('cardioWalking saved');
+// });
+// cardioStrengthCircuit.save( (err, cardioStrengthCircuit) => {
+//     if(err) console.error(err);
+//     console.log('cardioStrengthCircuit saved');
+// });
