3571a527800a92bb2b49be5f84f690c712e5a898
shaun <shauncarr2010@hotmail.com>
2020-01-07 12:15:32 -0600

    connection to atlas

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const MongoClient = require('mongodb').MongoClient;
+const config = require('/Users/laskey/Documents/Blue_Ocean/lvlUpFitness/DB/config.js')
 
 router.get('/', (req,res) => {
-    MongoClient.connect('mongodb://127.0.0.1:27017/lvlupdb', (err,client) => {
+    MongoClient.connect(`mongodb+srv://shauncarr22:${config}@lvlupfitdb-lef31.mongodb.net/test?retryWrites=true&w=majority`, (err,client) => {
         if(err) console.error(err);
-        const db = client.db('lvlupdb')
+        const db = client.db('test')
         db.collection('userprofiles').find().toArray( (err,result) => {
             if(err) console.error(err);
             res.send(result)
