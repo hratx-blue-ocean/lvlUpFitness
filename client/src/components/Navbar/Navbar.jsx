6253994df4cb9a13fa56202e1d5d2f468d287c43
monrning0wl <dahalsujanusa@gmail.com>
2020-01-07 02:14:53 -0600

    navbar complete

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from "react";
+import React, { Component, useContext, useState } from "react";
 import { AuthContext } from "../../AuthContext";
 import { Link, useHistory } from "react-router-dom";
 import { DebugRouter } from "../../App.jsx";
@@ -7,7 +7,10 @@ import {
   faHome,
   faDumbbell,
   faUtensils,
-  faCalendarAlt
+  faCalendarAlt,
+  faHeart,
+  faBackspace,
+  faSearch
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Navbar.css";
@@ -16,76 +19,78 @@ const Navbar = () => {
   let reRoute = useHistory();
   const context = useContext(AuthContext);
   const { isAuth } = context;
-  console.log(isAuth);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const SearchBar = () => {
+    return (
+      <div className="search-bar">
+        <input
+          type="text"
+          name="name"
+          value={searchQuery}
+          placeholder="Search this bro"
+          onChange={e => setSearchQuery(e.target.value)}
+        />
+        <div className="search-button" onClick={() => setSearchQuery("")}>
+          <FontAwesomeIcon icon={faSearch} size="2x" />
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="header-bar">
+      <div className="logo">
+        <Link to="/Navbar">
+          <FontAwesomeIcon
+            size="4x"
+            icon={faHeart}
+            fixedWidth={false}
+            transform=" right 1 rotate-350"
+          />
+        </Link>
+      </div>
+      <SearchBar />
+      <div className="message" >
+        <div>Welcome</div>
+        
+        <div>
+          <strong>Rodney</strong>
+        </div>
+      </div>
       
-      <ul className="icons-bar">
-        <li className="profile-icon">
-          <Link to="/">
-            <FontAwesomeIcon icon={faHome} size="2x" />
-            Profile
-          </Link>
-        </li>
-        <li className="dumbell-icon">
-          <Link to="/" >
-            <FontAwesomeIcon icon={faDumbbell} size="2x" />
-            Workout
-          </Link>
-        </li>
-        <li className="meal-icon">
-          <Link to="/">
-            <FontAwesomeIcon icon={faUtensils} size="2x" />
-            Meals
-          </Link>
-        </li>
-        <li className="calender-icon">
-          <Link to="/">
-            <FontAwesomeIcon icon={faCalendarAlt} size="2x" />
-            Schedule
-          </Link>
-        </li>
-      </ul>
+      <div className="search-icon">
+        
+          <FontAwesomeIcon icon={faSearch} size="2x" />
+          
+      </div>
+
+      <div className="profile-icon">
+        <Link to="/Profile">
+          <FontAwesomeIcon icon={faHome} size="2x" />
+          <p>Profile</p>
+        </Link>
+      </div>
+      <div className="dumbell-icon">
+        <Link to="/">
+          <FontAwesomeIcon icon={faDumbbell} size="2x" />
+          <p>Workout</p>
+        </Link>
+      </div>
+      <div className="meal-icon">
+        <Link to="/">
+          <FontAwesomeIcon icon={faUtensils} size="2x" />
+          <p>Meals</p>
+        </Link>
+      </div>
+      <div className="calender-icon">
+        <Link to="/">
+          <FontAwesomeIcon icon={faCalendarAlt} size="2x" />
+          <p>Schedule</p>
+        </Link>
+      </div>
     </div>
   );
-
-  // return (
-  //   <div className="header-bar">
-  //     <div
-  //       className="profile-icon"
-  //       onClick={() => {
-  //         reRoute.push("/Profile");
-  //       }}
-  //     >
-  //       <FontAwesomeIcon icon={faHome} size="2x" />
-  //     </div>
-  //     <div
-  //       className="dumbell-icon"
-  //       onClick={() => {
-  //         reRoute.push("/Profile");
-  //       }}
-  //     >
-  //       <FontAwesomeIcon icon={faDumbbell} size="2x" />
-  //     </div>
-  //     <div
-  //       className="meal-icon"
-  //       onClick={() => {
-  //         reRoute.push("/Profile");
-  //       }}
-  //     >
-  //       <FontAwesomeIcon icon={faUtensils} size="2x" />
-  //     </div>
-  //     <div
-  //       className="calender-icon"
-  //       onClick={() => {
-  //         reRoute.push("/Profile");
-  //       }}
-  //     >
-  //       <FontAwesomeIcon icon={faCalendarAlt} size="2x" />
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default Navbar;
