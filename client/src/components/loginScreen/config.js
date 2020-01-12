fe7b6b0462b6cd4ab2b8afde3900581516ace740
shaun <shauncarr2010@hotmail.com>
2020-01-12 13:10:23 -0600

    changes

---
+++ client/src/components/loginScreen/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBP5hioLuazEuXA2JbchOkUNPeKvfe9Jdo",
+    authDomain: "lvlupfitness-8ba78.firebaseapp.com",
+    databaseURL: "https://lvlupfitness-8ba78.firebaseio.com",
+    projectId: "lvlupfitness-8ba78",
+    storageBucket: "lvlupfitness-8ba78.appspot.com",
+    messagingSenderId: "941108051087",
+    appId: "1:941108051087:web:3094cd602f6fc00fa204fa",
+    measurementId: "G-W73S3Z78W2"
+  };
+export default firebaseConfig;
