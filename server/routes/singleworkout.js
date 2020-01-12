a5d95036c8da032147f63bc4c5adaaf7208b3d4d
shaun <shauncarr2010@hotmail.com>
2020-01-12 10:54:17 -0600

    route wip

--- server/routes/singleworkout.js
+++ server/routes/singleworkout.js
@@ -12,26 +12,13 @@ router.get("/", (req, res) => {
     (err, client) => {
       if (err) console.error(err);
       const db = client.db("test");
-    //   console.log(req.body.id);
+     //console.log(req.body.id);
       const findId = req.body.id
-      console.log(findId)
-      db.collection("bodyweights").find().toArray((err, result) => {
-          if (err) console.error(err);
-        //   console.log(result)
-          let newResult = []
-          let foundWorkout = []
-          for(let i = 0; i < result.length; i++){
-              for(let j = 0; j < result[i].type.length; j++){
-                if(result[i].type[j]._id === findId){
-                    newResult.push(result[i])
-                }
-                //   console.log(result[i].type[i]._id)
-                console.log(result[i].type[j]._id)
-            }
-        } 
-        res.send(newResult)
-        // res.send(result[0].type[0]._id)
-      });
+    //   console.log(findId)
+      db.collection("bodyweights").aggregate([{$unwind: "bodyweight"}, {$match: {"bodyweights._id": `"${findId}"`}}]).toArray((err,results) => {
+          if(err) console.error(err);
+          res.send(results)
+      })
     }
   );
 });
