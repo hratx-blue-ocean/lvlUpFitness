ed3b6b586a34e1716197cf62b3a85a23fd5f15ee
monrning0wl <dahalsujanusa@gmail.com>
2020-01-12 18:08:25 -0600

    add template calender

--- client/src/components/Meals/CustomMeal.jsx
+++ client/src/components/Meals/CustomMeal.jsx
@@ -3,34 +3,33 @@ import { Redirect } from "react-router-dom";
 import "./CustomMeal.css";
 
 const CustomMeal = ({ savedMeals }) => {
-  let mealData = savedMeals
+  let mealData = savedMeals;
   const [addMealClicked, setAddMeal] = useState(false);
   const [mealOfDay, setMealOfDay] = useState("No Meal Plan:");
   const [mealDescription, setMealDescription] = useState("Add Meal Plan:");
   const [propsExist, setPropsExist] = useState(false);
   const getData = arg => {
-    if (arg!==undefined) {
-      let num = Math.floor(Math.random() * arg.length);
+    if (arg) {
+      if (arg.length > 1) {
+        let num = Math.floor(Math.random() * arg.length);
 
-      let todayMeal = arg[num].name || "front end messed up";
+        let todayMeal = arg[num].name || "front end messed up";
 
-      setMealDescription(todayMeal);
-      setMealOfDay("Today's Meal:");
-      setPropsExist(true)
+        setMealDescription(todayMeal);
+        setMealOfDay("Today's Meal:");
+        setPropsExist(true);
+      }
     }
   };
   useEffect(() => {
     getData(mealData);
   }, [mealData]);
 
-
-
-
   if (addMealClicked) {
     return <Redirect to="/ListMealPlan" />;
   }
 
-  if (!propsExist){
+  if (!propsExist) {
     return (
       <div className="user-meal-tile">
         <div className="meal-day">{mealOfDay}</div>
@@ -44,8 +43,7 @@ const CustomMeal = ({ savedMeals }) => {
         </div>
       </div>
     );
-
-  } else{
+  } else {
     return (
       <div className="user-meal-tile">
         <div className="meal-day">{mealOfDay}</div>
@@ -62,9 +60,7 @@ const CustomMeal = ({ savedMeals }) => {
         </button>
       </div>
     );
-
   }
-  
 };
 
 export default CustomMeal;
